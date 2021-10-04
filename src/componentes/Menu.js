ea86b57e624300a2c5f0bb22b5a80124b26ccc77
Misael_ <mhernandezgo@minsait.com>
2021-10-04 10:17:32 -0500

    Se sube la primer version

---
+++ src/componentes/Menu.js
@@ -0,0 +1,25 @@
+const Menu = () => {
+  
+    return(
+           <>
+             <div className="padre"> 
+                  <div className="opciones">
+                      <h4>Boletas</h4>
+                  </div>
+
+                  <div className="opciones">
+                      <h4>Cardex</h4>
+                  </div>
+                  <div className="opciones">
+                      <h4>Constancias</h4>
+                  </div>  
+                
+              </div>  
+           </>
+
+    )
+
+}
+      
+export default Menu
+
